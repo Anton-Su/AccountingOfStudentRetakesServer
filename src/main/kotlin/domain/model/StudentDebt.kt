95783d52c56a1bed847dd9da7c4f836bb101d309
Antua <suleykin2005@mail.ru>
2026-05-28 02:44:54 +0300

    continue preporations for migration

--- src/main/kotlin/domain/model/StudentDebt.kt
+++ src/main/kotlin/domain/model/StudentDebt.kt
@@ -1,11 +1,11 @@
 package domain.model
 
-data class StudentDebt(
-    val debtId: Long,
-    val subjectId: Long,
-    val subjectTitle: String,
-    val teacherId: Long,
-    val createdAt: Long,
-    val status: DebtStatus,
-    val retakeId: Long? = null
-)
+//data class StudentDebt(
+//    val debtId: Long,
+//    val subjectId: Long,
+//    val subjectTitle: String,
+//    val teacherId: Long,
+//    val createdAt: Long,
+//    val status: DebtStatus,
+//    val retakeId: Long? = null
+//)
--- src/main/kotlin/domain/model/StudentSubject.kt
+++ src/main/kotlin/domain/model/StudentSubject.kt
@@ -6,6 +6,7 @@ data class StudentSubject(
     val id: Long,
     val studentId: Long,
     val subjectId: Long,
+    val subjectTitle: String,
     val status: StudentSubjectStatus,
     val score: Int? = null,
     val updatedAt: Instant
